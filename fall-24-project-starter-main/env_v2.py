030526d9a6f3a59f880833e1fc310456f4ad46e2
Jennifer Lee <jenni.lee1212@gmail.com>
2024-11-02 23:16:02 -0700

    Forgot to add implementation for the push and pop functions for regular block scope and also update set, get, create for the new scope approach

--- fall-24-project-starter-main/env_v2.py
+++ fall-24-project-starter-main/env_v2.py
@@ -39,60 +39,37 @@ class EnvironmentManager:
         else:
             self.block_stack = [{}] #or if the function stack is empty, reset to single global scope 
 
-
-    # Gets the data associated a variable name
-    # Update variable lookup: walk the enclosing chain innermost->outer until var found
-    def get(self, symbol):
-        if symbol in self.environment:
-            return self.environment[symbol]
-        elif self.enclosing: #if has an enclosing environment, check them inner->outer recursively until found
-            return self.enclosing.get(symbol)
-        else:
-            return None
-
-    # Sets the data associated with a variable name
-    # Update variable assignment: if variable ins't in this environment, walk enclosing chain inner->outer until var found and then assign
-    def set(self, symbol, value):
-        if symbol in self.environment:
-            self.environment[symbol] = value
-            return True
-        elif self.enclosing:
-            return self.enclosing.set(symbol, value) #recursively call set method until var found in enclosing
-        else:
-            return False
-
-    #not changed-- new var is always declared in the current innermost scope
-    def create(self, symbol, start_val):
-        if symbol not in self.environment: 
-          self.environment[symbol] = start_val 
-          return True
-        return False
+    def push_block_scope(self):
+        self.block_stack.append({}) #add new dict to current block stack (to isolate block scope)
     
-        
+    def pop_block_scope(self):
+        #remove most recent block dict from stack
+        self.block_stack.pop()
 
 
-
-'''
-class EnvironmentManager:
-    def __init__(self):
-        self.environment = {}
-
-    # Gets the data associated a variable name
     def get(self, symbol):
-        if symbol in self.environment:
-            return self.environment[symbol]
+        #check block and function scopes first *make sure going reversed so checking most recent
+        for scope in reversed(self.block_stack):
+            if symbol in scope:
+                return scope[symbol]
         return None
 
-    # Sets the data associated with a variable name
-    def set(self, symbol, value):
-        if symbol not in self.environment:
-            return False
-        self.environment[symbol] = value
-        return True
 
-    def create(self, symbol, start_val):
-        if symbol not in self.environment: 
-          self.environment[symbol] = start_val 
-          return True
+    def set(self, symbol, value):
+        #modify innermost scope where symbol is found
+        for scope in reversed(self.block_stack):
+            if symbol in scope:
+                scope[symbol] = value
+                return True
         return False
-'''
+
+
+    def create(self, symbol, value):
+        #check that variable is not already 
+        if symbol in self.block_stack[-1]:
+            return False
+        #then you can add it to scope
+        self.block_stack[-1][symbol] = value
+        return True
+    
+        
